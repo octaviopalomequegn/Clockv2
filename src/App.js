bd898bdd83ce69b4c06769ded753015945b533fa
Octavio Palomeque <octaviop_95@hotmail.com>
2019-02-04 15:46:45 -0600

    Cambio

--- src/App.js
+++ src/App.js
@@ -1,28 +1,49 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Component } from "react";
+import socketIOClient from "socket.io-client";
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      response: false,
+      endpoint: "http://127.0.0.1:4001"
+    };
+  }
+  componentDidMount() {
+    const { endpoint } = this.state;
+    const socket = socketIOClient(endpoint);
+    socket.on("TIME", data => this.setState({ response: data }));
+  }
+  
+  getLocalTime() {
+	const localDate = new Date();
+	var hora = localDate.getDate() + " del mes " + localDate.getMonth()+1 + " de " + localDate.getFullYear() +
+		" La hora es: " + localDate.getHours() + ":" + localDate.getMinutes() + ":" + localDate.getSeconds() + ":" + localDate.getMilliseconds();
+	return hora;
+  }
+  
   render() {
+    const { response } = this.state;
+	
+	const hora  = this.getLocalTime();
+	
+	
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div style={{textAlign: "center", color: "red" }}>
+	  
+        {response
+          ? <p>
+              La fecha y hora del servidor es: {response} <br/>
+			   
+            </p>
+			
+          : <p>Loading... </p>}
+		  
+		  <p>La fecha y hora del cliente es: {hora} </p>
       </div>
+	  
     );
   }
 }
+
 
 export default App;
